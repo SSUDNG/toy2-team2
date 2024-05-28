c016b1a7940e350c5a08d947c1b44a19a1347e87
Heejun Byeon <hejuby@gmail.com>
2024-05-28 13:13:09 +0900

    feat: add new font weights and change subset files to the whole font

--- src/style/font/index.ts
+++ src/style/font/index.ts
@@ -1,15 +1,42 @@
 import { createGlobalStyle } from 'styled-components';
-import PretendardBoldWoff2 from './Pretendard-Bold.subset.woff2';
-import PretendardRegularWoff2 from './Pretendard-Regular.subset.woff2';
-import PretendardBoldWoff from './Pretendard-Bold.subset.woff';
-import PretendardRegularWoff from './Pretendard-Regular.subset.woff';
+import PretendardExtraBoldWoff2 from './Pretendard-ExtraBold.woff2';
+import PretendardBoldWoff2 from './Pretendard-Bold.woff2';
+import PretendardSemiBoldWoff2 from './Pretendard-SemiBold.woff2';
+import PretendardMediumWoff2 from './Pretendard-Medium.woff2';
+import PretendardRegularWoff2 from './Pretendard-Regular.woff2';
+import PretendardExtraBoldWoff from './Pretendard-ExtraBold.woff';
+import PretendardBoldWoff from './Pretendard-Bold.woff';
+import PretendardSemiBoldWoff from './Pretendard-SemiBold.woff';
+import PretendardMediumWoff from './Pretendard-Medium.woff';
+import PretendardRegularWoff from './Pretendard-Regular.woff';
 
 export default createGlobalStyle`
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 800;
+    font-display: swap;
+    src: local('Pretendard Bold'), url(${PretendardExtraBoldWoff2}) format('woff2'), url(${PretendardExtraBoldWoff}) format('woff');
+  }
+
   @font-face {
     font-family: 'Pretendard';
     font-weight: 700;
     font-display: swap;
     src: local('Pretendard Bold'), url(${PretendardBoldWoff2}) format('woff2'), url(${PretendardBoldWoff}) format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-display: swap;
+    src: local('Pretendard Bold'), url(${PretendardSemiBoldWoff2}) format('woff2'), url(${PretendardSemiBoldWoff}) format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 500;
+    font-display: swap;
+    src: local('Pretendard Bold'), url(${PretendardMediumWoff2}) format('woff2'), url(${PretendardMediumWoff}) format('woff');
   }
 
   @font-face {
