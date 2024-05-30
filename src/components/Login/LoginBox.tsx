c65004058c1e20a825571a11b86453b6189a654b
BaDook2 <wkdrudqls114@gmail.com>
2024-05-30 13:13:10 +0900

    docs: delete formSchema.ts

--- src/components/Login/LoginBox.tsx
+++ src/components/Login/LoginBox.tsx
@@ -79,10 +79,11 @@ const ButtonBox = styled.div`
 `;
 
 const Input = styled.input`
-  ::placeholder {
-    color: ${(props) => props.theme.color};
+  &::placeholder {
+    color: ${(props) => props.theme.color.blue};
   }
-
+  font-size: ${(props) => props.theme.fontSize.title4};
   height: 4.63vh;
   padding: 0 0.74vh;
+  border: 1px solid ${(props) => props.theme.color.gray};
 `;
