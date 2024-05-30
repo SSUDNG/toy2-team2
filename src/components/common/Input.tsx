f0002564b3ef6c8b5d8709a1b90be8a6dc0e2b4e
BaDook2 <wkdrudqls114@gmail.com>
2024-05-30 14:28:30 +0900

    fix: color changes to placeholder color in common input component

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -6,7 +6,7 @@ type PlaceholderColor = 'black' | 'gray';
 type BorderColor = 'primary' | 'black' | 'red' | 'gray';
 
 interface InputStyleProps {
-  placeHolderColor: PlaceholderColor;
+  color: FontColor;
   borderColor: BorderColor;
 }
 
@@ -14,13 +14,13 @@ type InputProps<T extends object = Record<never, never>> =
   InputHTMLAttributes<HTMLInputElement> & InputStyleProps & T;
 
 function Input<T extends object>({
-  color = 'black',
+  placeholderColor = 'black',
   borderColor = 'black',
   type = 'text',
   ...props
 }: InputProps<T>) {
   return createElement(InputLayout, {
-    color,
+    placeholderColor,
     borderColor,
     ...props,
     type,
@@ -31,7 +31,7 @@ export default Input;
 
 const InputLayout = styled.input<InputStyleProps>`
   ${(props) =>
-    sb(props.placeHolderColor, {
+    sb(props.color, {
       black: css`
         &::placeholder {
           color: ${props.theme.color.black};
