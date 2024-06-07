decb8caf4af3a0b99b097102c1fac5a2b0cbba31
SSUDNG <ssudng2716@gmail.com>
2024-06-07 16:15:01 +0900

    fix: react import fix

--- src/components/Calendar/AddEventModal.tsx
+++ src/components/Calendar/AddEventModal.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
 import { setDoc, doc } from 'firebase/firestore';
--- src/components/Calendar/ColorOptions.tsx
+++ src/components/Calendar/ColorOptions.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 import { UseFormRegisterReturn } from 'react-hook-form';
 import theme from '../../style/theme';
